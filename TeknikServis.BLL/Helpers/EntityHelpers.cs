11c10db768c53adead3076f9089ac6bb2e7e70a5
ismail Çadır <ismailcadir@gmail.com>
2019-02-13 13:42:54 +0300

    Helpers Identity and entity ok.

---
+++ TeknikServis.BLL/Helpers/EntityHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Validation;
+using System.Linq;
+
+namespace TeknikServis.BLL.Helpers
+{
+    public class EntityHelpers
+    {
+        public static string ValidationMessage(DbEntityValidationException ex)
+        {
+            var result = ex.EntityValidationErrors.Aggregate(string.Empty, (accumulator2, validationErrors) => validationErrors.ValidationErrors.Aggregate(accumulator2, (accumulator, validationError) => accumulator += $"Property: {validationError.PropertyName} Error: {validationError.ErrorMessage}\n"));
+            return result + "\n";
+        }
+    }
+}
