d94696793bd9767b3c2b6a2eccc77a09a6897079
ismail Çadır <ismailcadir@gmail.com>
2019-02-14 12:12:05 +0300

    marka ok. UserControle Test ok.

---
+++ TeknikServis.DAL/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+namespace TeknikServis.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<TeknikServis.DAL.MyContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "TeknikServis.DAL.MyContext";
+        }
+
+        protected override void Seed(TeknikServis.DAL.MyContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
