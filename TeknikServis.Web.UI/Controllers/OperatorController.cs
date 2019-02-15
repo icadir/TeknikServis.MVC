faca0b3f2e05011b1982e99acb369393027ac57e
ismail ÇADIR <ismailcadir@gmail.com>
2019-02-16 00:23:59 +0300

    AriyaKabul Ekrani tasarım ok. Tamamlancak. #1

--- TeknikServis.Web.UI/Controllers/OperatorController.cs
+++ TeknikServis.Web.UI/Controllers/OperatorController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using AutoMapper;
 using TeknikServis.BLL.Repository;
+using TeknikServis.Entity.ViewModels;
 using TeknikServis.Entity.ViewModels.ArizaViewModels;
 
 namespace TeknikServis.Web.UI.Controllers
@@ -15,15 +17,31 @@ namespace TeknikServis.Web.UI.Controllers
         public ActionResult Index()
         {
             // TODO mapperları yapalım.
-            var data = new ArizaKayitRepo().GetAll(x => x.OperatorKabul == false).Select(x=>Mapper.Map<ArizaViewModel>(x)).ToList();
-         
+            var data = new ArizaKayitRepo().GetAll(x => x.OperatorKabul == false).Select(x => Mapper.Map<ArizaViewModel>(x)).ToList();
+
             return View(data);
         }
 
         [HttpGet]
-        public ActionResult ArizaDetay(string id)
+        public async Task<ActionResult> ArizaDetay(int id)
         {
-            return View();
+            try
+            {
+                var x = await new ArizaKayitRepo().GetByIdAsync(id);
+                var data = Mapper.Map<ArizaViewModel>(x);
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "Index",
+                    ControllerName = "Admin",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
